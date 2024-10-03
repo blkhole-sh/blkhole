909dca83b15056f869a25b8b61028a4af69ed094
Arian Gohari <a.gohari@startmunich.de>
2024-10-03 06:04:19 +0200

    Use cloudflare zero trust DNS

--- dns/dns.go
+++ dns/dns.go
@@ -33,7 +33,7 @@ func (h *dnsHandler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 
 	for _, question := range r.Question {
 		log.Printf(":53 received query: %s\n", question.Name)
-		answers, err := resolve(question.Name, question.Qtype, "194.242.2.9:853")
+		answers, err := resolve(question.Name, question.Qtype, "1.1.1.2:853")
 		if err != nil {
 			fmt.Println(err)
 		}
