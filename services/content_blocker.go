023d3561aa3ade592947b22ed7fd39c68e7aeac9
Arian Gohari <a.gohari@startmunich.de>
2024-10-04 09:41:52 +0200

    added more blocked DoH domains, check if question domain has trailing .localdomain suffix and strip if present

--- services/content_blocker.go
+++ services/content_blocker.go
@@ -21,15 +21,33 @@ func NewContentBlocker(scheduleRepo *repos.ScheduleRepoImpl) *ContentBlocker {
 // Define domainRegex to check for valid domains
 var domainRegex = regexp.MustCompile(`^(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$`)
 
+// Blocked DoH providers (in order to force Chromium to use Leo DoH)
+var dohBlocklist = []string{
+	"dns.google",
+	"dns.google.com",
+	"cloudflare-dns.com",
+	"one.one.one.one",
+	"dns.quad9.net",
+	"doh.opendns.com",
+	"dns.opendns.com",
+	"doh.cleanbrowsing.org",
+	"dns.nextdns.io",
+	"dns.adguard.com",
+	"doh.neustar.biz",
+	"doh.xfinity.com",
+	"doh.cira.ca",
+	"doh.yandex.net",
+	"doh.powerdns.org",
+	"dns.alidns.com",
+	"doh.dnssec.works",
+}
+
 // Hardcoded blocked domains (temporarily)
 var blockedDomains = []string{
 	"reddit.com",
 	"startmunich.de",
 	"youtube.com",
 	"dns.google",
-	"1.1.1.1",
-	"1.0.0.1",
-	"one.one.one.one",
 }
 
 // Check if a given domain is blocked
@@ -38,14 +56,17 @@ func (c *ContentBlocker) IsBlocked(domain string) (bool, error) {
 
 	// Check if domain is valid
 	if !domainRegex.MatchString(domain) {
-		return false, fmt.Errorf("%s is not a valid domain\n", domain)
+		return true, fmt.Errorf("%s is not a valid domain\n", domain)
 	}
 
 	// Normalize the domain to lowercase and remove trailing dot if present
 	domain = strings.ToLower(strings.TrimSuffix(domain, "."))
 
+	// Strip off .localdomain suffix if present
+	domain = strings.TrimSuffix(domain, ".localdomain")
+
 	// Check if domain is blocked
-	for _, blocked := range blockedDomains {
+	for _, blocked := range append(dohBlocklist, blockedDomains...) {
 		if domain == blocked || strings.HasSuffix(domain, "."+blocked) {
 			return true, nil
 		}
