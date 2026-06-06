5a8e85276ab2d98053ecc9c7f1422d8221bcfcaa
Arian Gohari <ariangohari1@gmail.com>
2026-06-06 17:22:07 +0200

    feat: hide edit button for default schedule

--- web/src/components/tile/ScheduleTile.tsx
+++ web/src/components/tile/ScheduleTile.tsx
@@ -144,16 +144,16 @@ export default function ScheduleTile(props: Props) {
 						</Show>
 					</div>
 				</div>
-				<div class="flex flex-row gap-6">
-					<ActionButton onclick={() => props.onEdit(props.schedule)}>
-						EDIT
-					</ActionButton>
-					<Show when={!props.schedule.isDefault}>
+				<Show when={!props.schedule.isDefault}>
+					<div class="flex flex-row gap-6">
+						<ActionButton onclick={() => props.onEdit(props.schedule)}>
+							EDIT
+						</ActionButton>
 						<ActionButton onclick={() => setDeleteOpen(true)}>
 							DELETE
 						</ActionButton>
-					</Show>
-				</div>
+					</div>
+				</Show>
 			</div>
 			<DeleteModal
 				open={deleteOpen()}
